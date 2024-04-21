65cc0e46a01ef30dc11f97c1f047a90b9994927a
pedgh87 <167669969+pedgh87@users.noreply.github.com>
2024-04-21 15:51:17 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '60286c4d-dbd0-450b-b9aa-da95cf0bf8b7';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
